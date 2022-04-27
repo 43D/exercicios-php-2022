d33b419d2d335f4383f04b075e82d34317f4a4e1
Allan Felipe <allan.goioere@hotmail.com>
2022-04-27 15:48:39 -0300

    Constutor Human

--- src/War/GamePlay/Country/HumanPlayerCountry.php
+++ src/War/GamePlay/Country/HumanPlayerCountry.php
@@ -5,4 +5,10 @@ namespace Galoa\ExerciciosPhp2022\War\GamePlay\Country;
 /**
  * Defines a country that is managed by a human player.
  */
-class HumanPlayerCountry extends BaseCountry {}
+class HumanPlayerCountry extends BaseCountry {
+
+    public function __construct(string $name)
+    {
+      parent::__construct($name);
+    }
+}
